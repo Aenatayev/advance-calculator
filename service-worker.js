62bdf1f975e52450778bf8cd6546bfc014cfadcd
Aenatayev <aenatayev@icloud.com>
2024-07-25 11:39:01 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = 'calculator-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
